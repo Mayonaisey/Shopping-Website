7d6214fceb96aa7ac4d694f7d5b7c51b23ecdea8
fhany2 <168221699+fhany2@users.noreply.github.com>
2024-06-23 19:06:43 +0200

    cart controllers ,checkout controllers

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,20 +1,20 @@
 
-function printError(id,msg){
-    document.getElementById(id).innerHTML=msg;
-}
-function change() {
-   xusername.innerHTML = user.username;
-   xemail.innerHTML = user.email;
-   xpassword.innerHTML = user.password;
-   xnumber.innerHTML = user.number;
-   xcountry.innerHTML = user.country;
-   xaddress.innerHTML = user.address;
-   xdetails.innerHTML = user.details;
+// function printError(id,msg){
+//     document.getElementById(id).innerHTML=msg;
+// }
+// function change() {
+//    xusername.innerHTML = user.username;
+//    xemail.innerHTML = user.email;
+//    xpassword.innerHTML = user.password;
+//    xnumber.innerHTML = user.number;
+//    xcountry.innerHTML = user.country;
+//    xaddress.innerHTML = user.address;
+//    xdetails.innerHTML = user.details;
 
-   document.getElementById('editButton').style.display = 'inline-block';
+//    document.getElementById('editButton').style.display = 'inline-block';
 
    
- }
+//  }
 window.onload=function(){
     const newValue=sessionStorage.getItem("newValue");
     document.getElementById("to").value=parseFloat(newValue).toFixed(1)+" EGP";
---
+++ routes/checkoutRoute.js
@@ -0,0 +1,27 @@
+
+// const express = require('express');
+// const router = express.Router();
+// const checkoutController = require('../controllers/checkoutController');
+
+// // // Route to render checkout page
+// // router.get('/', checkoutController.renderCheckoutPage);
+
+// // Route to view order summary
+// router.get('/summary', orderController.viewOrderSummary);
+
+// // Route to place order
+// router.post('/place', orderController.placeOrder);
+
+// module.exports = router;
+
+const express = require('express');
+const router = express.Router();
+const checkoutController = require('../controllers/checkoutController');
+
+// Route to display checkout page
+router.get('/', checkoutController.displayCheckout);
+
+// Route to place an order
+router.post('/placeOrder', checkoutController.placeOrder);
+
+module.exports = router;
